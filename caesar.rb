a7c8c7c532d7517801a9ba09c7432da10dbd1ec3
MikkoBoy <mikko.ahonen666@gmail.com>
2020-10-21 15:57:40 +0300

    Initial Commit

---
+++ caesar.rb
@@ -0,0 +1,14 @@
+
+def caesar_cipher(str, shift)
+  str.chars.map(&:ord).map do |char|
+    if char.between?(64,91)
+      ((char +shift) -65 ) % 26 + 65
+    elsif char.between?(96,123)
+      ((char +shift) -97 ) % 26 + 97
+    else
+      char
+    end
+  end.map(&:chr).join
+end
+
+puts caesar_cipher("ZzZ joo", 5)
